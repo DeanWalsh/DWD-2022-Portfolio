8d85e325497a4ef241e9b8514a57bc7c504f285d
DeanWalsh <deanochambers@gmail.com>
2022-06-08 04:58:45 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      '4xl': "1929px",
+    },
+    gridTemplateColumns: {
+      '1fr': '1fr 1fr',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
