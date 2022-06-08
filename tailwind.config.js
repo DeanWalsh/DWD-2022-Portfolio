171fc4ea2264307e89e39bba78c29c22316d8e13
DeanWalsh <deanochambers@gmail.com>
2022-06-08 17:09:05 +0100

    tailwind screen size updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,13 @@ module.exports = {
       center: true,
     },
     screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
       '4xl': { 'raw': '(min-height: 1230px)' },
-      'tall': { 'raw': '(min-height: 1230px)' } 
+      'tall': { 'raw': '(min-height: 1230px)' }, 
     },
     gridTemplateColumns: {
       '1fr': '1fr 1fr',
