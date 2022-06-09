2f153c4a99a9cb8e9b1413f92a5231d700328ff8
DeanWalsh <deanochambers@gmail.com>
2022-06-09 04:00:05 +0100

    further fixes

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
-let burger = document.getElementById('burger');
+const burger = document.getElementById('burger');
+const close = document.getElementById('close');
 const nav = document.getElementById('links');
 const socials = document.getElementById('socials');
 
 burger.addEventListener('click', () => {
-    nav.classList.add('nav-open'), nav.classList.add('animate__slideInRight');
-    socials.classList.add('socials-open'), socials.classList.add('animate__slideInRight');
-    setTimeout(() => {
-    burger.id = 'close';
-    }, 100);
-});
-
-
-
-
-document.addEventListener('click', (e) => {
-    if ((nav.classList.contains('nav-open')) && 
-        (e.target.tagname === 'links a' || e.target.tagname === 'socials a') ||
-        (e.target.id !== 'burger' && e.target.id !== 'links') || 
-        (e.target.id === 'close')) {
-
+    if (nav.classList.contains('nav-open')) {} else {
+        nav.classList.add('nav-open'), 
+        nav.classList.add('animate__slideInRight'), 
+        nav.classList.add('animate__animated');
+        socials.classList.add('socials-open'), 
+        socials.classList.add('animate__slideInRight'), 
+        socials.classList.add('animate__animated');
+        close.classList.add('close-open'), 
+        close.classList.add('animate__slideInRight'),
+        close.classList.add('animate__animated');
+    }
+    document.addEventListener('click', (e) => {
+        if (nav.classList.contains('nav-open') &&
+            (e.target.tagname === 'links a' || e.target.tagname === 'socials a') ||
+            (e.target.id !== 'burger' && e.target.id !== 'links') || 
+            (e.target.id === 'close')) {
+    
             nav.classList.add('animate__slideOutRight');
             socials.classList.add('animate__slideOutRight');
+            close.classList.add('animate__slideOutRight');
+    
             setTimeout(() => {
-            burger.id = 'burger';
-            }, 100);
-        setTimeout(() => {
-            nav.classList.remove('nav-open');
-            socials.classList.remove('socials-open');
-            nav.classList.remove('animate__slideOutRight');
-            socials.classList.remove('animate__slideOutRight');
-        }, 1500);
-    }
+                close.classList.remove('animate__slideOutRight');
+                close.classList.remove('animate__slideInRight');
+                close.classList.remove('close-open');
+                close.classList.remove('animate__animated');
+                nav.classList.remove('nav-open');
+                nav.classList.remove('animate__slideOutRight');
+                nav.classList.remove('animate__slideInRight');
+                nav.classList.remove('animate__animated');
+                socials.classList.remove('socials-open');
+                socials.classList.remove('animate__slideOutRight');
+                socials.classList.remove('animate__slideInRight');
+                socials.classList.remove('animate__animated');
+            }, 600);
+        }
+    });
 });
+
+
+
